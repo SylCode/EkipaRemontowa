7db3a2908223093924fb4fe609a94933e39bcd84
SylCode <noccolas@gmail.com>
2021-04-29 22:07:37 +0200

    Creaded Node backend API

--- EkipaRemontowa/src/app/io.service.ts
+++ EkipaRemontowa/src/app/io.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+// import * as fs from 'fs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IoService {
 
-  constructor(private httpClient: HttpClient) { 
+  constructor(private httpClient: HttpClient) {
+    //const fs = require('fs');
+    //var text = fs.readFileSync('../../assets/about.txt','utf8');
   }
 
   public getGalleryList(): Observable<Object>
