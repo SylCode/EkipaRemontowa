7fe696a23a460c9b63ecf11b18228d2617a6ba1e
Mykyta Bobrov <noccolas@gmail.com>
2021-07-22 09:45:43 +0200

    Added component activation

--- EkipaRemontowa/src/app/mainpage/mainpage/mainpage.component.ts
+++ EkipaRemontowa/src/app/mainpage/mainpage/mainpage.component.ts
@@ -10,6 +10,7 @@ import {
 } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ScrollableDirective } from 'src/app/scrollable.directive';
+import { IoService } from 'src/app/services/io.service';
 import { ScrollService } from 'src/scroll.service';
 import { HeaderComponent } from '../header/header.component';
 
@@ -20,22 +21,51 @@ import { HeaderComponent } from '../header/header.component';
 })
 export class MainpageComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChildren(ScrollableDirective)
+  set ele1 (v: QueryList<ScrollableDirective>){
+    console.log('This element is set when ngIf = true');
+    this.scrollableComponentList = v;
+  };
+  @ViewChild(HeaderComponent) set ele2(v:HeaderComponent){
+    console.log('This element is set when ngIf = true');
+    this.header = v;
+  };
+  @ViewChild('wrapper') set ele3(v:ElementRef){
+    console.log('This element is set when ngIf = true');
+    this.basicScrollElRef = v;
+  };
+
   scrollableComponentList!: QueryList<ScrollableDirective>;
-  @ViewChild(HeaderComponent) header!: HeaderComponent;
-  @ViewChild('wrapper') basicScrollElRef!: ElementRef;
-
+  header!: HeaderComponent;
+  basicScrollElRef!: ElementRef;
   sub: Subscription = new Subscription();
+  
+  private ActiveModules: Array<string> = new Array<string>();
 
-  constructor(private scrollService: ScrollService) {}
+  constructor(private scrollService: ScrollService,
+    private ioService: IoService) {}
 
   ngOnInit(): void {
     this.scrollService.enableScroll();
+    this.ioService.getActiveModules()?.subscribe((res) => {
+      this.ActiveModules = res;
+    });
+    console.log('++++ngOnInit++++');
+    console.log(this.scrollableComponentList);
+    console.log(this.basicScrollElRef);
   }
 
   ngAfterViewInit(): void {
+    console.log('++++ngAfterViewInit++++');
     this.scrollService.scrollList = this.scrollableComponentList.toArray();
     this.scrollService.containerRef = this.basicScrollElRef;
+    console.log(this.scrollableComponentList);
+    console.log(this.basicScrollElRef);
   }
+
+  public moduleActive(name: string): boolean {
+    return this.ActiveModules.indexOf(name) > -1;
+  }
+
 
   ngOnDestroy() {}
 }
