43dc923efdca344aef707114a3519aad98cb7bfa
SylCode <noccolas@gmail.com>
2023-01-22 15:00:26 +0100

    Fixed styling

--- EkipaRemontowa/src/app/mainpage/footer/footer.component.ts
+++ EkipaRemontowa/src/app/mainpage/footer/footer.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  private currentYear!: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public getCurrentYear() {
+    return this.currentYear;
   }
 
+  constructor() {
+    var date = new Date(Date.now());
+    this.currentYear = date.getUTCFullYear();
+  }
+
+  ngOnInit(): void {}
 }
